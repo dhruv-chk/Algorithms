746a9d914d76cd197593df56244243528ebd7d71
dhruv-chk <dhruv.chk9@gmail.com>
2020-05-04 00:47:27 +0530

    Longest Substring Without Repeating Characters LeetCode.

---
+++ LongestSubstringWithoutRepeatingCharacters.py
@@ -0,0 +1,20 @@
+# Question link - https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+s = "abcabcbb"
+
+i = 0 
+j = 0
+res = 0
+
+hash_set = set()
+
+while j<len(s):
+    if s[j] not in hash_set:
+        hash_set.add(s[j])
+        j += 1
+        res = max(len(hash_set), res)
+    else:
+        hash_set.discard(s[j])
+        i += 1
+
+print(res)
