29148b16c01ebae000c5e47d41871ff869ca78ee
dhruvchakraborty <dhruv@coiempowerment.com>
2021-02-19 13:49:42 +0530

    test commit

--- Recursion/TowerOfHanoi.java
+++ Recursion/TowerOfHanoi.java
@@ -1,11 +1,6 @@
 public class TowerOfHanoi {
 
     public static int toh(int num){
-        if (num==0)
-            return 0;
-        else{
-            return toh(num/10) + (num%10);
-        }
         
     }
     public static void main(String[] args) {
